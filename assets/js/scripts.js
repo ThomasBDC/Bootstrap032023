53c89a287bd75ab4e0ef970f378924bfe63704e7
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2023-03-29 11:27:42 +0200

    Add elements bootstrap

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,18 @@
 const listCategories = document.querySelector(".list-categories");
 const listArticles = document.querySelector("#allArticles");
-const categorie =`<div class="categorie">
-                    <a>Ma categorie</a>
-                </div>`;
+const categorie =` <a href="#" class="list-group-item list-group-item-action">Philips</a>`;
 
-const article = `<article class="col-12 col-md-6 col-xl-4">
-                    <div>
-                        <img src="https://picsum.photos/300/150"/>
+const article = `<article class="col-12 col-md-6 col-xl-4 p-2">
+                    <div class="card shadow">
+                        <img class="bd-placeholder-img card-img-top" width="100%" src="https://picsum.photos/300/150" role="img">
+                        <div class="card-body">
+                            <h5 class="card-title">Microphone Rode NT1-A</h5>
+                            <p class="card-text">Avec sa capsule de condensateur de 1 pouce et sa réponse en fréquence étendue ...</p>
+                            <a href="#" class="btn btn-primary"  data-bs-toggle="tooltip" data-bs-placement="bottom" 
+                            data-bs-title="15% de réduction si vous l'achetez dans les 5 minutes">
+                            <i class="bi bi-cart"></i> Acheter</a>
+                        </div>
                     </div>
-                    <h2>Microphone Rode NT1-A</h2>
-                    <p>Avec sa capsule de condensateur de 1 pouce et sa réponse en fréquence étendue ...</p>
                 </article>`;
 
 
@@ -17,10 +20,14 @@ let htmlCategories = '';
 for(let i =0; i<6;i++){
     htmlCategories += categorie;
 }
-listCategories.innerHTML = htmlCategories;
+listCategories.innerHTML += htmlCategories;
 
 let htmlArticles = '';
 for(let i =0; i<12; i++){
     htmlArticles += article;
 }
 listArticles.innerHTML = htmlArticles;
+
+//Initialisation des tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
